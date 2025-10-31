29a543a3a607b58b686719cd7b82bc87ce405919
rboudrouss <r.boudrouss@gmail.com>
2025-10-31 12:48:01 +0100

    chore: more complex example in main

--- main.ts
+++ main.ts
@@ -1,11 +1,27 @@
-import { computePageRank, Graph } from "./src/pagerank.ts";
+import { computePageRank, type Graph } from "./src/pagerank.ts";
 
 const graph: Graph = {
-  nodes: [0, 1],
+  nodes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   edges: [
     [0, 1],
-    [1, 1],
-    [1, 0],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+    [1, 2],
+    [1, 5],
+    [2, 3],
+    [2, 6],
+    [3, 4],
+    [3, 7],
+    [4, 5],
+    [4, 8],
+    [5, 6],
+    [5, 9],
+    [6, 7],
+    [7, 8],
+    [8, 9],
+    [9, 0],
+    [9, 1],
   ],
 };
 
